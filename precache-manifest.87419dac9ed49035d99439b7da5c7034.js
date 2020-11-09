f23ee4320c460e5861bdf05c4ff4180db4df9de0
mirsat1 <66675314+mirsat1@users.noreply.github.com>
2020-11-09 14:38:52 +0100

    Updates

---
+++ precache-manifest.87419dac9ed49035d99439b7da5c7034.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292fb4fc7347a4424ca9eccb35830b11",
+    "url": "/articonf2/index.html"
+  },
+  {
+    "revision": "a45e5abe23590d6ffec9",
+    "url": "/articonf2/static/css/2.231051f4.chunk.css"
+  },
+  {
+    "revision": "a45e5abe23590d6ffec9",
+    "url": "/articonf2/static/js/2.75b6caad.chunk.js"
+  },
+  {
+    "revision": "7fb39dca80014fe6fa71a3eacb208152",
+    "url": "/articonf2/static/js/2.75b6caad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ae14b74ad8703af9b5f",
+    "url": "/articonf2/static/js/main.ab8cfaee.chunk.js"
+  },
+  {
+    "revision": "ce5c6b596bb013a8ad55",
+    "url": "/articonf2/static/js/runtime-main.a6b06181.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/articonf2/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/articonf2/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/articonf2/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/articonf2/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/articonf2/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/articonf2/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/articonf2/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/articonf2/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/articonf2/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/articonf2/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/articonf2/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/articonf2/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/articonf2/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/articonf2/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/articonf2/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/articonf2/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
