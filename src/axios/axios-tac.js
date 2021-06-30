fc676410cc5e41591b8d0f168736d813783b6819
Mirsat Sefidanoski <mirsat1994@gmail.com>
2021-06-30 07:44:17 +0200

    - M -

--- src/axios/axios-tac.js
+++ src/axios/axios-tac.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    // baseURL: 'http://15.237.93.29:8082/'
-    baseURL: 'http://localhost:9200/'
+    baseURL: 'http://15.237.93.29:8082/'
+    // baseURL: 'http://localhost:9200/'
 })
 
 export default instance
