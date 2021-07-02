3f15b8b91b2c74be2cbc0bd9857d8562a951273a
mirsat1 <mirsat.sefidanoski@cse.uist.edu.mk>
2021-07-02 22:43:33 +0200

    added some changes to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,12 +15,13 @@ export default function Header() {
     return (
         <div>
             <div className="container blue circleBehind">
-                <Button 
+                <Button
+                    style={{backgroundColor: "rgb(25, 114, 245)", color: "white"}} 
                     content='Toggle menu' 
                     icon={showMenu ? 'left arrow' : 'right arrow'} 
                     labelPosition={showMenu ? 'left' : 'right'} 
                     floated="left" 
-                    onClick={toggleMenu} 
+                    onClick={toggleMenu}
                 />
                 <Link to="/beta/testing" style={{textAlign: "center"}}><img src="https://i.imgur.com/tgkmCTc.png" alt="articonflogo" /></Link>
                 {/* <Link to="/beta/testing"className="a" title="navHome">Home</Link>
@@ -42,6 +43,7 @@ export default function Header() {
                 } */}
             </div>
             <Sidebar
+                style={{backgroundColor: "#2a388f"}}
                 as={Menu}
                 animation={"push"}
                 direction={"left"}
@@ -50,49 +52,49 @@ export default function Header() {
                 visible={showMenu}
             >
                 <Link to="/beta/testing"className="a" title="navHome">
-                    <Menu.Item as='a'>
+                    <Menu.Item as='a' style={{color: "white"}}>
                         <Icon name='home' />
                         Home
                     </Menu.Item>
                 </Link>
                 {UCProvider && <Link to="/beta/testing/deploy" className="a" title="deployLink">
-                        <Menu.Item as='a'>
+                        <Menu.Item as='a' style={{color: "white"}}>
                             <Icon name='cloud upload' />
                             Deploy
                         </Menu.Item>
                     </Link>}
                 {UCProvider && <Link to="/beta/testing/dashboard" className="a" title="deployLink">
-                    <Menu.Item as='a'>
+                    <Menu.Item as='a' style={{color: "white"}}>
                         <Icon name='wrench' />
                         Advance <br />Configuration
                     </Menu.Item>
                 </Link>}
                 {UCProvider && <Link to="/beta/testing/deployed" className="a" title="deployLink">
-                    <Menu.Item as='a'>
+                    <Menu.Item as='a' style={{color: "white"}}>
                         <Icon name='lab' />
                         Find & Test<br />Topology<br />
                     </Menu.Item>
                 </Link>}
                 <Link to="/beta/testing/bank"className="a" title="navHome">
-                    <Menu.Item as='a'>
+                    <Menu.Item as='a' style={{color: "white"}}>
                         <Icon name='chart pie' />
                         Analitycs<br /> & <br />Visualisations
                     </Menu.Item>
                 </Link>
                 <Link to="/beta/testing/monitoring"className="a" title="navHome">
-                    <Menu.Item as='a'>
+                    <Menu.Item as='a' style={{color: "white"}}>
                         <Icon name='chart line' />
                         Monitoring
                     </Menu.Item>
                 </Link>
                 <Link to="/beta/testing/contact"className="a" title="navHome">
-                    <Menu.Item as='a'>
+                    <Menu.Item as='a' style={{color: "white"}}>
                         <Icon name='paper plane' />
                         Contact Us
                     </Menu.Item>
                 </Link>
                 <Link to="/beta/testing/userprofile"className="a" title="navHome">
-                    <Menu.Item as='a'>
+                    <Menu.Item as='a' style={{color: "white"}}>
                         <Icon name='user' />
                         User Profile
                     </Menu.Item>
@@ -103,7 +105,7 @@ export default function Header() {
                     initialiseIdsOnSignOut();
                     usersRef.remove();
                 }}>
-                    <Menu.Item as='a'>
+                    <Menu.Item as='a' style={{color: "white"}}>
                         <Icon name='power' />
                         Sign Out
                     </Menu.Item>
@@ -111,12 +113,12 @@ export default function Header() {
                 :
                 <Link to="/login" className="a">
                     <Menu.Item as='a'>
-                        <Icon name='power' />
+                        <Icon name='power' style={{color: "white"}}/>
                         Sign In
                     </Menu.Item>
                 </Link>
                 }
-                <Menu.Item as='a' onClick={toggleMenu}>
+                <Menu.Item as='a' onClick={toggleMenu} style={{color: "white"}}>
                     <Icon name='angle double left' />
                     Collapse menu
                 </Menu.Item>
